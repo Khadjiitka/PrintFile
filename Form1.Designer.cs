8dc32ee4f0a73b03e7035fb66e5757f44b0351e9
Khadjiitka <viculia.kravtsova@gmail.com>
2025-05-24 15:35:56 +0300

    Add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            printToolStripMenuItem = new ToolStripMenuItem();
+            exiteToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            textBox1 = new TextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, printToolStripMenuItem, exiteToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(224, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // printToolStripMenuItem
+            // 
+            printToolStripMenuItem.Name = "printToolStripMenuItem";
+            printToolStripMenuItem.Size = new Size(224, 26);
+            printToolStripMenuItem.Text = "Print";
+            printToolStripMenuItem.Click += printToolStripMenuItem_Click;
+            // 
+            // exiteToolStripMenuItem
+            // 
+            exiteToolStripMenuItem.Name = "exiteToolStripMenuItem";
+            exiteToolStripMenuItem.Size = new Size(224, 26);
+            exiteToolStripMenuItem.Text = "Exite";
+            exiteToolStripMenuItem.Click += exiteToolStripMenuItem_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(46, 155);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(701, 93);
+            textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private OpenFileDialog openFileDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private TextBox textBox1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem printToolStripMenuItem;
+        private ToolStripMenuItem exiteToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing.Printing;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Програма дає змогу відкрити в стандартному діалозі текстовий файл,
+        // переглянути його в текстовому полі без можливості зміни тексту (ReadOnly) 
+        // і, за бажання користувача, вивести цей текст на принтер
+        System.IO.StreamReader Reader;
+        public Form1()
+        {
+            InitializeComponent();
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Open/Print Text File";
+            textBox1.Clear();
+            openFileDialog1.FileName = null;
+
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Reader = new System.IO.StreamReader(openFileDialog1.FileName, System.Text.Encoding.GetEncoding(1251));
+                try
+                {
+                    printDocument1.Print();
+                }
+                finally
+                {
+                    Reader.Close();
+                }
+            }
+            catch (Exception Status)
+            {
+                MessageBox.Show(Status.Message);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстові файли (*.txt)|*.txt|All files (*.*)|*.*"; 
+            openFileDialog1.ShowDialog();
+            if (string.IsNullOrEmpty(openFileDialog1.FileName)) return;
+            try
+            {
+                // Створення потоку StreamReader для читання з файлу
+                Reader = new System.IO.StreamReader(openFileDialog1.FileName, System.Text.Encoding.GetEncoding(1251));
+                // замовлення кодової сторінки Win1251 для російських літер
+                textBox1.Text = Reader.ReadToEnd();
+                Reader.Close();
+            }
+            catch (System.IO.FileNotFoundException Status)
+            {
+                MessageBox.Show(Status.Message +
+                "\nНемає такого файлу", "Помилка",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception Status)
+            {
+                // Звіт про інші помилки:
+                MessageBox.Show(Status.Message, "Помилка",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Single RowsОnPage = 0.0F;
+            Single Y = 0;
+            var LeftEdge = e.MarginBounds.Left;
+            var UpperEdge = e.MarginBounds.Top;
+            var Rows = String.Empty;
+            var Font = new Font("Times New Roman", 12.0F);
+            // Обчислюємо кількість рядків на одній сторінці
+            RowsОnPage = e.MarginBounds.Height /
+            Font.GetHeight(e.Graphics);
+            var i = 0; // рахунок рядків
+            while (i < RowsОnPage)
+            {
+                Rows = Reader.ReadLine();
+                if (Rows == null) break;
+                // Для VB: If Рядок Is Nothing Then Exit While
+                Y = UpperEdge + i * Font.GetHeight(e.Graphics);
+                // Друк рядка
+                e.Graphics.DrawString(Rows, Font, Brushes.Black,
+                LeftEdge, Y, new StringFormat());
+                i += 1;
+            }
+            // Друк наступної сторінки, якщо є ще рядки файлу
+            if (Rows != null) e.HasMorePages = true;
+            // Для VB: If Рядок <> Null Then ...
+            else e.HasMorePages = false;
+        }
+
+        private void exiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
